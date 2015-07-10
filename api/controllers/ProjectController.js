49707418aa74eb9055cb9770a7e0bb1893c9b2af
Luis Arce <luis.arce@jalasoft.com>
2015-07-09 20:52:19 -0400

    Updated the method findAll to return all projects of the database

--- api/controllers/ProjectController.js
+++ api/controllers/ProjectController.js
@@ -9,19 +9,25 @@ module.exports = {
     create: function(req, res) {
         var params = req.params.all()
         project.create({
-            name: 'project name'
+            name: params.name
         }).exec(function createCB(err, created) {
             return res.json({
                 notice: 'Created user with name ' + created.name
             });
         });
-
     },
 
     getAll: function(req, res) {
+        project.find().exec(function(err, val) {
+            res.send(val);
+        });
+    },
+
+    getAllThatContains: function(req, res) {
+        var params = req.params.all()
         project.find({
             name: {
-                'contains': 'name'
+                'contains': params.contains
             }
         }).exec(function(err, val) {
             res.send(val);

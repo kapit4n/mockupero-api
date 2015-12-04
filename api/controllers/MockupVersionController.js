d8ec5b6dfaae8f33488c4bbf0f724e4899ab8f6a
Luis Arce <luis.arce@jalasoft.com>
2015-12-03 22:10:52 -0400

    Adding Solution to work with versions

---
+++ api/controllers/MockupVersionController.js
@@ -0,0 +1,28 @@
+/**
+ * MockupVersionController
+ *
+ * @description :: Server-side logic for managing mockupversions
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	saveIt: function(req, res) {
+        var versionRecord = req.params.all();
+        if (req.isSocket && req.method === 'POST') {
+            MockupVersion.create(versionRecord)
+                .exec(function(error, created1) {
+                    console.log(created1);
+                    MockupVersion.publishCreate({
+                        id: created1.id,
+                        mockupId: created1.mockupId,
+                        number: created1.number
+                    });
+                    return res.send('created a version');
+                });
+        } else if (req.isSocket) {
+            sails.sockets.join(req.socket, roomName);
+            return res.send('User subscribed to creation');
+        }
+    }
+};
+

f27450b9a2421b3f4e6639aad0fb1b69a624e32a
Luis Arce <luis.arce@jalasoft.com>
2015-12-03 22:20:58 -0400

    Some changes to work with get and post method for the mockupEditor sockets

--- api/controllers/MockupEditorController.js
+++ api/controllers/MockupEditorController.js
@@ -6,65 +6,89 @@
  */
 module.exports = {
     editors: function(req, res) {
-        username_val = req.param('username');
-        user.find().where({
-            username: username_val
-        }).exec(function(err1, foundUser) {
-            if (err1) {
-                console.log('Error to query User');
-                console.log(err1);
-            } else {
-                if (foundUser.length == 0) {
-                    return res.send('There is not user with this username: ' + username_val);
-                }
-                console.log('Success user query');
-                console.log(foundUser.length);
-                MockupEditor.find().where({
-                    username: username_val
-                }).exec(function(err2, foundLogin) {
-                    if (err2) {
-                        console.log('Error to query MockupEditor');
-                        console.log(err2);
-                    } else {
-                        console.log('Success MockupEditor query');
-                        console.log(foundLogin.length);
-                        if (foundLogin.length == 0) {
-                            MockupEditor.create({
-                                userId: foundUser[0].id,
-                                username: foundUser[0].username,
-                                online: true
-                            }).exec(function(err3, createdLog) {
-                                console.log(err3);
-                                console.log(createdLog);
-                                return res.send(createdLog[0]);
-                            });
-                        } else {
-                            MockupEditor.update({
-                                id: foundLogin[0].id
-                            }, {
-                                online: true
-                            }).exec(function afterwards(err, updated) {
 
-                                if (err) {
-                                    console.log('Error to put online the user');
-                                    return res.send('failed');
-                                }
-                                console.log('The status of the MockupEditor is ' + updated[0].online);
-                                MockupEditor.publishUpdate(updated[0].id, {
-                                    online: updated[0].online,
-                                    username: updated[0].username
-                                });
-                                sails.sockets.broadcast('MockupEditor', {
-                                    value: updated[0]
-                                });
-                                return res.send(updated[0]);
-                            });
-                            console.log('The record has been updated');
-                        }
+        var data_from_client;
+        var roomName;
+        data_from_client = req.params.all();
+        console.log(data_from_client);
+
+        if (data_from_client && data_from_client['roomName']) {
+            roomName = data_from_client['roomName'];
+        } else {
+            roomName = 'General_Mockup_Room';
+        }
+
+        if (!req.isSocket) {
+            return res.send('is not socket');
+        }
+
+        if (req.isSocket && req.method === 'POST') {
+            username_val = req.param('username');
+            user.find().where({
+                username: username_val
+            }).exec(function(err1, foundUser) {
+                if (err1) {
+                    console.log('Error to query User');
+                    console.log(err1);
+                } else {
+                    if (foundUser.length == 0) {
+                        return res.send('There is not user with this username: ' + username_val);
                     }
-                });
-            }
-        });
+                    console.log('Success user query');
+                    console.log(foundUser.length);
+                    MockupEditor.find().where({
+                        username: username_val
+                    }).exec(function(err2, foundLogin) {
+                        if (err2) {
+                            console.log('Error to query MockupEditor');
+                            console.log(err2);
+                        } else {
+                            console.log('Success MockupEditor query');
+                            console.log(foundLogin.length);
+                            if (foundLogin.length == 0) {
+                                MockupEditor.create({
+                                    userId: foundUser[0].id,
+                                    username: foundUser[0].username,
+                                    online: true
+                                }).exec(function(err3, createdLog) {
+                                    console.log(err3);
+                                    console.log(createdLog);
+                                    return res.send(createdLog[0]);
+                                });
+                            } else {
+                                MockupEditor.update({
+                                    id: foundLogin[0].id
+                                }, {
+                                    online: true
+                                }).exec(function afterwards(err, updated) {
+
+                                    if (err) {
+                                        console.log('Error to put online the user');
+                                        return res.send('failed');
+                                    }
+                                    console.log('The status of the MockupEditor is ' + updated[0].online);
+                                    MockupEditor.publishUpdate(updated[0].id, {
+                                        online: updated[0].online,
+                                        username: updated[0].username
+                                    });
+                                    sails.sockets.broadcast('MockupEditor', {
+                                        value: updated[0]
+                                    });
+                                    return res.send(updated[0]);
+                                });
+                                console.log('The record has been updated');
+                            }
+                        }
+                    });
+                }
+            });
+        } else {
+            MockupEditor.watch(req.socket);
+            sails.sockets.join(req.socket, roomName);
+            return res.send('User subscribed to ' + roomName);
+        }
+
+        
     },
     logout: function(req, res) {
         if (req.isSocket) {

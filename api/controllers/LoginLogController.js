b260fa134bf4f09b38b78898d3a5dc26cdc05faf
Luis Arce <luis.arce@jalasoft.com>
2016-03-02 20:12:49 -0400

    Login Log Updates

--- api/controllers/LoginLogController.js
+++ api/controllers/LoginLogController.js
@@ -6,6 +6,10 @@
  */
 module.exports = {
     login: function(req, res) {
+        var socketId = '';
+        if (req.isSocket) {
+            socketId = sails.sockets.id(req.socket);
+        }
         username_val = req.param('username');
         user.find().where({
             username: username_val
@@ -46,9 +50,9 @@ module.exports = {
                                     //console.log(('Error to put online the user');
                                     return res.send('failed');
                                 }
-                                //console.log(('The status of the loginLog is ' + updated[0].online);
+                                console.log('The status of the loginLog is ' + updated[0].online);
                                 LoginLog.publishUpdate(updated[0].id, {online: updated[0].online, username: updated[0].username});
-                                //sails.sockets.broadcast('loginLog', { value: updated[0] });
+                                sails.sockets.broadcast('loginLog', { value: updated[0] });
                                 return res.send(updated[0]);
                             });
                             //console.log(('The record has been updated');
@@ -64,24 +68,27 @@ module.exports = {
             //console.log((username_val);
             user.find().where({
                 username: username_val
-            }).exec(function(err1, foundLogin) {
-                if (foundLogin.length > 0) {
-                    LoginLog.update({
-                        userId: foundLogin[0].id
-                    }, {
-                        online: false
-                    }).exec(function afterwards(err, updated) {
-                        if (err) {
-                            //console.log(('Error to put logout the user');
-                            return res.send('error');
-                        }
-                        //console.log(('The status of the loginLog is' + updated[0].online);
-                        //console.log((updated[0].id);
-                        LoginLog.publishUpdate(updated[0].id, {online: false, username: updated[0].username});
-                        //sails.sockets.broadcast('loginLog', { value: updated[0] });
-                        return res.send(updated[0]);
-                    });
-                } else {
+            }).exec(function(err1, foundLoginList) {
+                if (foundLoginList.length > 0) {
+                    for (i = 0, len = foundLoginList.length; i < len; i++) {
+                        console.log('This is the example');
+                        var foundLogin = foundLoginList[0];
+                        LoginLog.update({
+                            userId: foundLogin.id
+                        }, {
+                            online: false
+                        }).exec(function afterwards(err, updated) {
+                            if (err) {
+                                return res.send('error');
+                            }
+                            // this event is not working ok
+                            LoginLog.publishUpdate(updated.id, {online: false, username: updated.username});
+                            sails.sockets.broadcast('loginLog', { value: updated });
+                            return res.send(updated);
+                        });
+                    }
+                }
+                else {
                     return res.send('');
                 }
             });

564262e4e2c95345085a8217d73221fdd1b50d1e
Luis Arce <luis.arce@jalasoft.com>
2016-03-15 20:28:05 -0400

    Updated the DB models to work with permissions

---
+++ api/controllers/RoleController.js
@@ -0,0 +1,11 @@
+/**
+ * RoleController
+ *
+ * @description :: Server-side logic for managing roles
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+

1d1c8766e1f7cb97b7a6320214a066e6cf88e5a1
Luis Arce <amigox100pre111@gmail.com>
2016-09-26 19:57:03 -0400

    Updated the project to integrate with passport

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -16,7 +16,7 @@ module.exports.bootstrap = function bootstrap(next) {
    * It's very important to trigger this 'next' method when you are finished with the bootstrap!
    * (otherwise your server will never lift, since it's waiting on the bootstrap)
    */
-  sails.services.passport.loadStrategies();
+  //sails.services.passport.loadStrategies();
 
   next();
 };

97c5fdc93d9c1e746d1cb4a1455c97d99691928e
larce <luis.arce22@gmail.com>
2017-03-23 11:30:11 -0400

    Add owner field in mockup model

--- api/models/mockup.js
+++ api/models/mockup.js
@@ -28,6 +28,9 @@ module.exports = {
         project: {
             model: 'project'
         },
+        owner: {
+            model: 'user'
+        },
         feature: {
           model: 'feature'
         }
